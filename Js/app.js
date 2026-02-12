836ea0fc98a155ed93e63ea11e85d25e2270313f
CARSPECX <nkshri24@gmail.com>
2026-02-12 15:18:56 +0530

    Create app.js

---
+++ Js/app.js
@@ -0,0 +1,20 @@
+// Load all cars globally
+let CARS = [];
+
+// Fetch cars once
+fetch("data/cars.json")
+  .then(res => res.json())
+  .then(data => {
+      CARS = data;
+      console.log("Cars loaded:", CARS.length);
+  });
+
+// Utility: get car by ID
+function getCarById(id){
+    return CARS.find(c => c.id == id);
+}
+
+// Utility: format price
+function formatPrice(p){
+    return "₹" + p.toLocaleString();
+}
